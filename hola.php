f6d9e9f7b260b0b7885ed48fefbe286140589e2e
LorenaMartinAlvaro <lormaralv@gmail.com>
2024-02-07 19:22:27 +0000

    Incluyo el primer fichero

---
+++ hola.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title> LORENA MARTIN ALVARO 50546471S</title>
+	</head>
+	<body>
+		<p>Contenido de la página Web</p>
+		<?php echo "Servidor: " . $_SERVER["SERVER_SOFTWARE"].
+		"<br>"; echo "Raíz: " . $_SERVER["DOCUMENT_ROOT"] . "<br>";
+		?>
+	</body>
+
+	<footer>LORENA MARTIN ALVARO 50546471S</footer>
+</html>
